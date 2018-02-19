1df0b074bdfb64b054f427e7dbb3240981c73d17
Patrick-Ranjit D. Madsen <prm@trustpilot.com>
2018-02-19 16:18:12 +0100

    Add data_pagerduty_team data source

---
+++ pagerduty/data_pagerduty_team.go
@@ -0,0 +1,58 @@
+package pagerduty
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/heimweh/go-pagerduty/pagerduty"
+)
+
+func dataSourcePagerDutyTeam() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourcePagerDutyTeamRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Required: false,
+			},
+		},
+	}
+}
+
+func dataSourcePagerDutyTeamRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*pagerduty.Client)
+
+	log.Printf("[INFO] Reading PagerDuty team")
+
+	searchTeam := d.Get("name").(string)
+
+	resp, _, err := client.Teams.List(&pagerduty.ListTeamsOptions{})
+	if err != nil {
+		return err
+	}
+
+	var found *pagerduty.Team
+
+	for _, team := range resp.Teams {
+		if team.Name == searchTeam {
+			found = team
+			break
+		}
+	}
+
+	if found == nil {
+		return fmt.Errorf("Unable to locate any team with name: %s", searchTeam)
+	}
+
+	d.SetId(found.ID)
+	d.Set("name", found.Name)
+	d.Set("description", found.Description)
+
+	return nil
+}

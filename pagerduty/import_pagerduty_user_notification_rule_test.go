92b604c29a78fd96742e3df97d514362827db136
Jacek Kikiewicz <jacek.kikiewicz@competec.ch>
2019-04-12 09:58:32 +0200

    Feature: Resource User Notification Rule

---
+++ pagerduty/import_pagerduty_user_notification_rule_test.go
@@ -0,0 +1,34 @@
+package pagerduty
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccPagerDutyUserNotificationRule_import(t *testing.T) {
+	contactMethodType := "phone_contact_method"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckPagerDutyUserDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckPagerDutyUserNotificationRuleContactMethodConfig(contactMethodType),
+			},
+			{
+				ResourceName:      "pagerduty_user_notification_rule.foo",
+				ImportStateIdFunc: testAccCheckPagerDutyUserNotificationRuleId,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckPagerDutyUserNotificationRuleId(s *terraform.State) (string, error) {
+	return fmt.Sprintf("%v:%v", s.RootModule().Resources["pagerduty_user.foo"].Primary.ID, s.RootModule().Resources["pagerduty_user_notification_rule.foo"].Primary.ID), nil
+}

6ce1d0ccc04798d3dabd9df8ef8503046f133e4a
Daisuke Fujita <dtanshi45@gmail.com>
2019-09-19 19:25:02 +0900

    Remain existing import acctest and add new one

--- pagerduty/import_pagerduty_team_membership_test.go
+++ pagerduty/import_pagerduty_team_membership_test.go
@@ -11,6 +11,28 @@ import (
 func TestAccPagerDutyTeamMembership_import(t *testing.T) {
 	user := fmt.Sprintf("tf-%s", acctest.RandString(5))
 	team := fmt.Sprintf("tf-%s", acctest.RandString(5))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckPagerDutyTeamMembershipDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckPagerDutyTeamMembershipConfig(user, team),
+			},
+
+			{
+				ResourceName:      "pagerduty_team_membership.foo",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccPagerDutyTeamMembership_importWithRole(t *testing.T) {
+	user := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	team := fmt.Sprintf("tf-%s", acctest.RandString(5))
 	role := "manager"
 
 	resource.Test(t, resource.TestCase{
@@ -19,7 +41,7 @@ func TestAccPagerDutyTeamMembership_import(t *testing.T) {
 		CheckDestroy: testAccCheckPagerDutyTeamMembershipDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckPagerDutyTeamMembershipConfig(user, team, role),
+				Config: testAccCheckPagerDutyTeamMembershipWithRoleConfig(user, team, role),
 			},
 
 			{

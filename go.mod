1f302cfbb95199014063283644e5eb77469e656b
Scott McAllister <smcallister@pagerduty.com>
2020-04-28 18:06:38 -0700

    adding priority data source

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/terraform-providers/terraform-provider-pagerduty
 require (
 	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/hashicorp/terraform-plugin-sdk v1.7.0
-	github.com/heimweh/go-pagerduty v0.0.0-20200415004831-bc2b3224a572
+	github.com/heimweh/go-pagerduty v0.0.0-20200429000711-fdd3a48907e7
 )
 
 go 1.13
---
+++ pagerduty/data_source_pagerduty_priority_test.go
@@ -0,0 +1,52 @@
+package pagerduty
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDataSourcePagerDutyPriority_Basic(t *testing.T) {
+	dataSourceName := "data.pagerduty_priority.p1"
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourcePagerDutyPriorityConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, "id"),
+					resource.TestCheckResourceAttr(dataSourceName, "name", "P1"),
+				),
+			},
+		},
+	})
+}
+func TestAccDataSourcePagerDutyPriority_P2(t *testing.T) {
+	dataSourceName := "data.pagerduty_priority.p2"
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourcePagerDutyP2Config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, "id"),
+					resource.TestCheckResourceAttr(dataSourceName, "name", "P2"),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataSourcePagerDutyPriorityConfig = `
+data "pagerduty_priority" "p1" {
+  name = "p1"
+}
+`
+
+const testAccDataSourcePagerDutyP2Config = `
+data "pagerduty_priority" "p2" {
+  name = "p2"
+}
+`

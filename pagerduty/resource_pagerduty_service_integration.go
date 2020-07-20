b96eedea6f3635caebb3085920edc6bda90341bb
ehlerorngard <eorngard@pagerduty.com>
2020-07-20 12:08:36 -0500

    adjust retry timeout

--- pagerduty/resource_pagerduty_service_integration.go
+++ pagerduty/resource_pagerduty_service_integration.go
@@ -115,7 +115,7 @@ func resourcePagerDutyServiceIntegrationCreate(d *schema.ResourceData, meta inte
 
 	service := d.Get("service").(string)
 
-	retryErr := resource.Retry(15*time.Second, func() *resource.RetryError {
+	retryErr := resource.Retry(1*time.Minute, func() *resource.RetryError {
 		if serviceIntegration, _, err := client.Services.CreateIntegration(service, serviceIntegration); err != nil {
 			if isErrCode(err, 400) {
 				time.Sleep(2 * time.Second)

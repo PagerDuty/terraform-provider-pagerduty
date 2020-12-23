68a158db13217c2af8f87af4d5a35e6b1f8a29f7
Stefano Boriero <stefano.boriero@ocado.com>
2020-12-23 20:03:22 +0100

    style: format code

--- pagerduty/data_source_pagerduty_business_service.go
+++ pagerduty/data_source_pagerduty_business_service.go
@@ -33,13 +33,13 @@ func dataSourcePagerDutyBusinessServiceRead(d *schema.ResourceData, meta interfa
 	return resource.Retry(2*time.Minute, func() *resource.RetryError {
 		resp, _, err := client.BusinessServices.List()
 		if err != nil {
-			if (isErrCode(err, 429)) {
+			if isErrCode(err, 429) {
 				// Delaying retry by 30s as recommended by PagerDuty
 				// https://developer.pagerduty.com/docs/rest-api-v2/rate-limiting/#what-are-possible-workarounds-to-the-events-api-rate-limit
 				time.Sleep(30 * time.Second)
 				return resource.RetryableError(err)
 			}
-			
+
 			return resource.NonRetryableError(err)
 		}
 
@@ -57,11 +57,11 @@ func dataSourcePagerDutyBusinessServiceRead(d *schema.ResourceData, meta interfa
 				fmt.Errorf("Unable to locate any business service with the name: %s", searchName),
 			)
 		}
-	
+
 		d.SetId(found.ID)
 		d.Set("name", found.Name)
 
 		return nil
 	})
-	
+
 }
--- pagerduty/data_source_pagerduty_ruleset.go
+++ pagerduty/data_source_pagerduty_ruleset.go
@@ -33,13 +33,13 @@ func dataSourcePagerDutyRulesetRead(d *schema.ResourceData, meta interface{}) er
 	return resource.Retry(2*time.Minute, func() *resource.RetryError {
 		resp, _, err := client.Rulesets.List()
 		if err != nil {
-			if (isErrCode(err, 429)) {
+			if isErrCode(err, 429) {
 				// Delaying retry by 30s as recommended by PagerDuty
 				// https://developer.pagerduty.com/docs/rest-api-v2/rate-limiting/#what-are-possible-workarounds-to-the-events-api-rate-limit
 				time.Sleep(30 * time.Second)
 				return resource.RetryableError(err)
 			}
-			
+
 			return resource.NonRetryableError(err)
 		}
 

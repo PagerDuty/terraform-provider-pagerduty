509242d015db7f49ec01ef9ef137dc7ca0c17fe8
Stefano Boriero <stefano.boriero@ocado.com>
2020-12-23 18:18:56 +0100

    Retry datasources on throttling
    feat: adding retries to all datasources

--- pagerduty/data_source_pagerduty_vendor.go
+++ pagerduty/data_source_pagerduty_vendor.go
@@ -47,8 +47,14 @@ func dataSourcePagerDutyVendorRead(d *schema.ResourceData, meta interface{}) err
 	return resource.Retry(2*time.Minute, func() *resource.RetryError {
 		resp, _, err := client.Vendors.List(o)
 		if err != nil {
-			time.Sleep(2 * time.Second)
-			return resource.RetryableError(err)
+			if (isErrCode(err, 429)) {
+				// Delaying retry by 30s as recommended by PagerDuty
+				// https://developer.pagerduty.com/docs/rest-api-v2/rate-limiting/#what-are-possible-workarounds-to-the-events-api-rate-limit
+				time.Sleep(30 * time.Second)
+				return resource.RetryableError(err)
+			}
+			
+			return resource.NonRetryableError(err)
 		}
 
 		var found *pagerduty.Vendor

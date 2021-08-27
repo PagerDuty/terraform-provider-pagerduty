961f74e0339f618e1bf2af4570c6220c694c41c9
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2021-08-27 10:55:26 +0800

    user: Validate role against known role names

--- pagerduty/resource_pagerduty_user.go
+++ pagerduty/resource_pagerduty_user.go
@@ -44,6 +44,16 @@ func resourcePagerDutyUser() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 				Default:  "user",
+				ValidateFunc: validateValueFunc([]string{
+					"admin",
+					"limited_user",
+					"observer",
+					"owner",
+					"read_only_user",
+					"restricted_access",
+					"read_only_limited_user",
+					"user",
+				}),
 			},
 
 			"job_title": {

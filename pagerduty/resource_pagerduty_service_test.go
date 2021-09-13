ed4046c949ce8cc4474725c1155a304a470f403d
Dominik Rastawicki <drastawi@gmail.com>
2021-09-13 17:07:38 +0800

    Ensure general urgency rule is not set for an urgency rule of type support hours

--- pagerduty/resource_pagerduty_service_test.go
+++ pagerduty/resource_pagerduty_service_test.go
@@ -3,6 +3,7 @@ package pagerduty
 import (
 	"fmt"
 	"log"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -314,6 +315,10 @@ func TestAccPagerDutyService_BasicWithIncidentUrgencyRules(t *testing.T) {
 					resource.TestCheckResourceAttr(
 						"pagerduty_service.foo", "support_hours.0.type", "fixed_time_per_day"),
 				),
+			},
+			{
+				Config:      testAccCheckPagerDutyServiceWithIncidentUrgencyRulesConfigError(username, email, escalationPolicy, serviceUpdated),
+				ExpectError: regexp.MustCompile("general urgency cannot be set for a use_support_hours incident urgency rule type"),
 			},
 			{
 				Config: testAccCheckPagerDutyServiceWithIncidentUrgencyRulesWithoutScheduledActionsConfig(username, email, escalationPolicy, service),
@@ -914,6 +919,71 @@ resource "pagerduty_service" "foo" {
 	incident_urgency_rule {
 		type = "use_support_hours"
 
+		during_support_hours {
+			type    = "constant"
+			urgency = "high"
+		}
+		outside_support_hours {
+			type    = "constant"
+			urgency = "low"
+		}
+	}
+
+	support_hours {
+		type         = "fixed_time_per_day"
+		time_zone    = "America/Lima"
+		start_time   = "09:00:00"
+		end_time     = "17:00:00"
+		days_of_week = [ 1, 2, 3, 4, 5 ]
+	}
+
+	scheduled_actions {
+		type = "urgency_change"
+		to_urgency = "high"
+		at {
+			type = "named_time"
+			name = "support_hours_start"
+		}
+	}
+}
+`, username, email, escalationPolicy, service)
+}
+
+func testAccCheckPagerDutyServiceWithIncidentUrgencyRulesConfigError(username, email, escalationPolicy, service string) string {
+	return fmt.Sprintf(`
+resource "pagerduty_user" "foo" {
+	name        = "%s"
+	email       = "%s"
+	color       = "green"
+	role        = "user"
+	job_title   = "foo"
+	description = "foo"
+}
+
+resource "pagerduty_escalation_policy" "foo" {
+	name        = "%s"
+	description = "bar"
+	num_loops   = 2
+
+	rule {
+		escalation_delay_in_minutes = 10
+		target {
+			type = "user_reference"
+			id   = pagerduty_user.foo.id
+		}
+	}
+}
+
+resource "pagerduty_service" "foo" {
+	name                    = "%s"
+	description             = "foo"
+	auto_resolve_timeout    = 1800
+	acknowledgement_timeout = 1800
+	escalation_policy       = pagerduty_escalation_policy.foo.id
+
+	incident_urgency_rule {
+		type    = "use_support_hours"
+		urgency = "high"
 		during_support_hours {
 			type    = "constant"
 			urgency = "high"

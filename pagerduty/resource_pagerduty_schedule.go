80cf11180123e4c3bf7f69499ce545783eb02fe4
Dominik Rastawicki <drastawi@gmail.com>
2021-10-09 09:49:45 +0800

    Add validation on schedule layer turn rotation length

--- pagerduty/resource_pagerduty_schedule.go
+++ pagerduty/resource_pagerduty_schedule.go
@@ -97,6 +97,7 @@ func resourcePagerDutySchedule() *schema.Resource {
 						"rotation_turn_length_seconds": {
 							Type:     schema.TypeInt,
 							Required: true,
+							ValidateFunc: validation.IntBetween(3600, 365 * 24 * 3600),
 						},
 
 						"users": {

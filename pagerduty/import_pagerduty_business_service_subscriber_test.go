8c6dec7a718628a7d38c44ab28a17c40bcad08d5
Gavin Reynolds <greynolds@pagerduty.com>
2021-11-05 12:03:03 +0000

    Add business service subscriber import and create subscriber complex ID for business service, subscriber type and subscriber id
    Signed-off-by: Gavin Reynolds <greynolds@pagerduty.com>

---
+++ pagerduty/import_pagerduty_business_service_subscriber_test.go
@@ -0,0 +1,36 @@
+package pagerduty
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccPagerDutyBusinessServiceSubscriber_import(t *testing.T) {
+	businessServiceName := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	team := fmt.Sprintf("tf-%s", acctest.RandString(5))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckPagerDutyBusinessServiceSubscriberDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckPagerDutyBusinessServiceSubscriberTeamConfig(businessServiceName, team),
+			},
+			{
+				ResourceName:      "pagerduty_business_service_subscriber.foo",
+				ImportStateIdFunc: testAccCheckPagerDutyBusinessServiceSubscriberID,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckPagerDutyBusinessServiceSubscriberID(s *terraform.State) (string, error) {
+	return fmt.Sprintf("%v.%v.%v", s.RootModule().Resources["pagerduty_business_service.foo"].Primary.ID, "team", s.RootModule().Resources["pagerduty_team.foo"].Primary.ID), nil
+}

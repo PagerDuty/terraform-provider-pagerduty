3b0372f6f2559708b7b9509cecaa4275d41d59f2
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2021-12-24 18:54:54 +0800

    schedule: Validate daily/weekly restrictions
    Do not allow a daily restriction that spans a full 24*time.Hour
    and similar for weekly restrictions. The PD API rejects 24*time.Hour
    and 24*7*time.Hour for the API.

--- pagerduty/resource_pagerduty_schedule.go
+++ pagerduty/resource_pagerduty_schedule.go
@@ -25,8 +25,15 @@ func resourcePagerDutySchedule() *schema.Resource {
 				rn := diff.Get(fmt.Sprintf("layer.%d.restriction.#", li)).(int)
 				for ri := 0; ri <= rn; ri++ {
 					t := diff.Get(fmt.Sprintf("layer.%d.restriction.%d.type", li, ri)).(string)
+					d := diff.Get(fmt.Sprintf("layer.%d.restriction.%d.duration_seconds", li, ri)).(int)
 					if t == "daily_restriction" && diff.Get(fmt.Sprintf("layer.%d.restriction.%d.start_day_of_week", li, ri)).(int) != 0 {
 						return fmt.Errorf("start_day_of_week must only be set for a weekly_restriction schedule restriction type")
+					}
+					if t == "weekly_restriction" && d >= int((time.Hour*24*7).Seconds()) {
+						return fmt.Errorf("weekly restriction must be less than a week: %v", (time.Hour * 24 * 7).Seconds())
+					}
+					if t == "daily_restriction" && d >= int((time.Hour*24).Seconds()) {
+						return fmt.Errorf("daily restriction must be less than a day: %v", (time.Hour * 24).Seconds())
 					}
 				}
 			}

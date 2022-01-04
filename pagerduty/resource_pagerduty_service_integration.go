8d17403eb8d7c1fd39f3c70dcfa3a5cbeb5ef8af
Yannic Bonenberger <yannic@engflow.com>
2022-01-04 09:47:15 +0100

    buildServiceIntegrationStruct: check for integration type

--- pagerduty/resource_pagerduty_service_integration.go
+++ pagerduty/resource_pagerduty_service_integration.go
@@ -2,6 +2,7 @@ package pagerduty
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"strings"
@@ -12,6 +13,10 @@ import (
 	"github.com/heimweh/go-pagerduty/pagerduty"
 )
 
+const (
+	errEmailIntegrationMustHaveEmail = "integration_email attribute must be set for an integration type generic_email_inbound_integration"
+)
+
 func resourcePagerDutyServiceIntegration() *schema.Resource {
 	return &schema.Resource{
 		Create: resourcePagerDutyServiceIntegrationCreate,
@@ -20,8 +25,8 @@ func resourcePagerDutyServiceIntegration() *schema.Resource {
 		Delete: resourcePagerDutyServiceIntegrationDelete,
 		CustomizeDiff: func(context context.Context, diff *schema.ResourceDiff, i interface{}) error {
 			t := diff.Get("type").(string)
-			if t == "generic_email_inbound_integration" && diff.Get("integration_email").(string) == "" {
-				return fmt.Errorf("integration_email attribute must be set for an integration type generic_email_inbound_integration")
+			if t == "generic_email_inbound_integration" && diff.Get("integration_email").(string) == "" && diff.NewValueKnown("integration_email") {
+				return errors.New(errEmailIntegrationMustHaveEmail)
 			}
 			return nil
 		},
@@ -82,7 +87,7 @@ func resourcePagerDutyServiceIntegration() *schema.Resource {
 	}
 }
 
-func buildServiceIntegrationStruct(d *schema.ResourceData) *pagerduty.Integration {
+func buildServiceIntegrationStruct(d *schema.ResourceData) (*pagerduty.Integration, error) {
 	serviceIntegration := &pagerduty.Integration{
 		Name: d.Get("name").(string),
 		Type: "service_integration",
@@ -111,7 +116,11 @@ func buildServiceIntegrationStruct(d *schema.ResourceData) *pagerduty.Integratio
 		}
 	}
 
-	return serviceIntegration
+	if serviceIntegration.Type == "generic_email_inbound_integration" && serviceIntegration.IntegrationEmail == "" {
+		return nil, errors.New(errEmailIntegrationMustHaveEmail)
+	}
+
+	return serviceIntegration, nil
 }
 
 func fetchPagerDutyServiceIntegration(d *schema.ResourceData, meta interface{}, errCallback func(error, *schema.ResourceData) error) error {
@@ -163,7 +172,10 @@ func fetchPagerDutyServiceIntegration(d *schema.ResourceData, meta interface{}, 
 func resourcePagerDutyServiceIntegrationCreate(d *schema.ResourceData, meta interface{}) error {
 	client, _ := meta.(*Config).Client()
 
-	serviceIntegration := buildServiceIntegrationStruct(d)
+	serviceIntegration, err := buildServiceIntegrationStruct(d)
+	if err != nil {
+		return err
+	}
 
 	log.Printf("[INFO] Creating PagerDuty service integration %s", serviceIntegration.Name)
 
@@ -198,7 +210,10 @@ func resourcePagerDutyServiceIntegrationRead(d *schema.ResourceData, meta interf
 func resourcePagerDutyServiceIntegrationUpdate(d *schema.ResourceData, meta interface{}) error {
 	client, _ := meta.(*Config).Client()
 
-	serviceIntegration := buildServiceIntegrationStruct(d)
+	serviceIntegration, err := buildServiceIntegrationStruct(d)
+	if err != nil {
+		return err
+	}
 
 	service := d.Get("service").(string)
 

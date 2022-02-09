620023697e8a914065ab5167909835820efa6d8c
DevOps Rob <robert.raybarnes@gmail.com>
2022-02-09 11:02:53 +0000

    Update pagerduty/resource_pagerduty_webhook_subscription.go
    Co-authored-by: Shohei Maeda <11495867+smaeda-ks@users.noreply.github.com>

--- pagerduty/resource_pagerduty_webhook_subscription.go
+++ pagerduty/resource_pagerduty_webhook_subscription.go
@@ -223,7 +223,7 @@ func expandDeliveryMethod(v interface{}) pagerduty.DeliveryMethod {
 		TemporarilyDisabled: dmMap["temporarily_disabled"].(bool),
 		Type:                dmMap["type"].(string),
 		URL:                 dmMap["url"].(string),
-		CustomHeaders: 		 list,
+		CustomHeaders: 		 headers,
 	}
 	return method
 }

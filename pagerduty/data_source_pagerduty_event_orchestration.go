d1fc25bfa351fc496a369cb2a766387abbb60371
Pari Dhanakoti <pdhanakoti@pagerduty.com>
2022-05-05 16:24:50 -0400

    add datasource event orchestration

---
+++ pagerduty/data_source_pagerduty_event_orchestration.go
@@ -0,0 +1,106 @@
+package pagerduty
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/heimweh/go-pagerduty/pagerduty"
+)
+
+func dataSourcePagerDutyEventOrchestration() *schema.Resource {
+	return &schema.Resource{
+		Read:   dataSourcePagerDutyEventOrchestrationRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"routes": {
+				Type: schema.TypeInt,
+				Computed: true,
+			},
+			"integrations": {
+				Type: schema.TypeList,
+				Computed: true,
+				Optional: true, // Tests keep failing if "Optional: true" is not provided
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"parameters": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"routing_key": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"type": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourcePagerDutyEventOrchestrationRead(d *schema.ResourceData, meta interface{}) error {
+	client, err := meta.(*Config).Client()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[INFO] Reading PagerDuty Event Orchestration")
+
+	searchName := d.Get("name").(string)
+
+	return resource.Retry(5*time.Minute, func() *resource.RetryError {
+		resp, _, err := client.EventOrchestrations.List()
+		if err != nil {
+			if isErrCode(err, 429) {
+				// Delaying retry by 30s as recommended by PagerDuty
+				// https://developer.pagerduty.com/docs/rest-api-v2/rate-limiting/#what-are-possible-workarounds-to-the-events-api-rate-limit
+				time.Sleep(30 * time.Second)
+				return resource.RetryableError(err)
+			}
+
+			return resource.NonRetryableError(err)
+		}
+
+		var found *pagerduty.EventOrchestration
+
+		for _, orchestration := range resp.Orchestrations {
+			if orchestration.Name == searchName {
+				found = orchestration
+				break
+			}
+		}
+
+		if found == nil {
+			return resource.NonRetryableError(
+				fmt.Errorf("Unable to locate any Event Orchestration with the name: %s", searchName),
+			)
+		}
+
+		d.SetId(found.ID)
+		setEventOrchestrationProps(d, found)
+
+		return nil
+	})
+}

d80bf6632c2effba53aedb818d3cc2f446f8464e
Alena Pantuzenko <apantuzenko@pagerduty.com>
2022-05-06 15:40:19 -0400

    add team checks to the tests

--- pagerduty/import_pagerduty_event_orchestration_test.go
+++ pagerduty/import_pagerduty_event_orchestration_test.go
@@ -11,7 +11,8 @@ import (
 func TestAccPagerDutyEventOrchestration_import(t *testing.T) {
 	name := fmt.Sprintf("tf-name-%s", acctest.RandString(5))
 	description := fmt.Sprintf("tf-description-%s", acctest.RandString(5))
-	teamName := fmt.Sprintf("tf-team-%s", acctest.RandString(5))
+	team1 := fmt.Sprintf("tf-team1-%s", acctest.RandString(5))
+	team2 := fmt.Sprintf("tf-team2-%s", acctest.RandString(5))
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -19,7 +20,7 @@ func TestAccPagerDutyEventOrchestration_import(t *testing.T) {
 		CheckDestroy: testAccCheckPagerDutyEventOrchestrationDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckPagerDutyEventOrchestrationConfig(name, description, teamName),
+				Config: testAccCheckPagerDutyEventOrchestrationConfig(name, description, team1, team2),
 			},
 			{
 				ResourceName:      "pagerduty_event_orchestration.foo",

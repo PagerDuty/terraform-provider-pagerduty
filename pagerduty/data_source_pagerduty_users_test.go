468969f97eebe3ded84d3ed2633d86c64e320bf2
José Antonio Reyes <imjaroiswebdev@gmail.com>
2022-07-18 13:14:53 -0400

    add test case for querying with more than one team

--- pagerduty/data_source_pagerduty_users_test.go
+++ pagerduty/data_source_pagerduty_users_test.go
@@ -10,31 +10,47 @@ import (
 )
 
 func TestAccDataSourcePagerDutyUsers_Basic(t *testing.T) {
-	teamname := fmt.Sprintf("tf-team-%s", acctest.RandString(5))
+	teamname1 := fmt.Sprintf("tf-team-%s", acctest.RandString(5))
+	teamname2 := fmt.Sprintf("tf-team-%s", acctest.RandString(5))
 	username1 := fmt.Sprintf("tf-user-%s", acctest.RandString(5))
 	email1 := fmt.Sprintf("%s@foo.test", username1)
 	username2 := fmt.Sprintf("tf-user-%s", acctest.RandString(5))
 	email2 := fmt.Sprintf("%s@foo.test", username2)
+	username3 := fmt.Sprintf("tf-user-%s", acctest.RandString(5))
+	email3 := fmt.Sprintf("%s@foo.test", username3)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourcePagerDutyUsersConfig(teamname, username1, email1, username2, email2),
+				Config: testAccDataSourcePagerDutyUsersConfig(teamname1, teamname2, username1, email1, username2, email2, username3, email3),
 				Check: resource.ComposeTestCheckFunc(
 					testAccDataSourcePagerDutyUsersExists("data.pagerduty_users.test_all_users"),
-					testAccDataSourcePagerDutyUsersExists("data.pagerduty_users.test_by_team"),
+					testAccDataSourcePagerDutyUsersExists("data.pagerduty_users.test_by_1_team"),
+					testAccDataSourcePagerDutyUsersExists("data.pagerduty_users.test_by_2_team"),
 					resource.TestCheckResourceAttrSet(
 						"data.pagerduty_users.test_all_users", "users.#"),
 					resource.TestCheckResourceAttr(
-						"data.pagerduty_users.test_by_team", "users.#", "1"),
+						"data.pagerduty_users.test_by_1_team", "users.#", "1"),
 					resource.TestCheckResourceAttrSet(
-						"data.pagerduty_users.test_by_team", "users.0.id"),
+						"data.pagerduty_users.test_by_1_team", "users.0.id"),
 					resource.TestCheckResourceAttr(
-						"data.pagerduty_users.test_by_team", "users.0.name", username2),
+						"data.pagerduty_users.test_by_1_team", "users.0.name", username2),
 					resource.TestCheckResourceAttr(
-						"data.pagerduty_users.test_by_team", "users.0.email", email2),
+						"data.pagerduty_users.test_by_1_team", "users.0.email", email2),
+					resource.TestCheckResourceAttr(
+						"data.pagerduty_users.test_by_2_team", "users.#", "2"),
+					resource.TestCheckResourceAttrSet(
+						"data.pagerduty_users.test_by_2_team", "users.0.id"),
+					resource.TestCheckResourceAttr(
+						"data.pagerduty_users.test_by_2_team", "users.0.name", username2),
+					resource.TestCheckResourceAttr(
+						"data.pagerduty_users.test_by_2_team", "users.0.email", email2),
+					resource.TestCheckResourceAttr(
+						"data.pagerduty_users.test_by_2_team", "users.1.name", username3),
+					resource.TestCheckResourceAttr(
+						"data.pagerduty_users.test_by_2_team", "users.1.email", email3),
 				),
 			},
 		},
@@ -55,31 +71,49 @@ func testAccDataSourcePagerDutyUsersExists(n string) resource.TestCheckFunc {
 	}
 }
 
-func testAccDataSourcePagerDutyUsersConfig(teamname, username1, email1, username2, email2 string) string {
+func testAccDataSourcePagerDutyUsersConfig(teamname1, teamname2, username1, email1, username2, email2, username3, email3 string) string {
 	return fmt.Sprintf(`
-    resource "pagerduty_team" "test" {
+    resource "pagerduty_team" "test1" {
         name        = "%s"
     }
+    resource "pagerduty_team" "test2" {
+        name        = "%s"
+    }
+
     resource "pagerduty_user" "test_wo_team" {
       name = "%s"
       email = "%s"
     }
-    resource "pagerduty_user" "test_w_team" {
+    resource "pagerduty_user" "test_w_team1" {
       name = "%s"
       email = "%s"
     }
-    resource "pagerduty_team_membership" "test" {
-      team_id = pagerduty_team.test.id
-      user_id = pagerduty_user.test_w_team.id
+    resource "pagerduty_user" "test_w_team2" {
+      name = "%s"
+      email = "%s"
+    }
+
+    resource "pagerduty_team_membership" "test1" {
+      team_id = pagerduty_team.test1.id
+      user_id = pagerduty_user.test_w_team1.id
+    }
+    resource "pagerduty_team_membership" "test2" {
+      depends_on = [pagerduty_team_membership.test1]
+      team_id = pagerduty_team.test2.id
+      user_id = pagerduty_user.test_w_team2.id
     }
 
     data "pagerduty_users" "test_all_users" {
-      depends_on = [pagerduty_user.test_w_team, pagerduty_user.test_wo_team]
+      depends_on = [pagerduty_user.test_w_team1, pagerduty_user.test_wo_team]
     }
 
-    data "pagerduty_users" "test_by_team" {
-      depends_on = [pagerduty_team_membership.test]
-      team_ids = [pagerduty_team.test.id]
+    data "pagerduty_users" "test_by_1_team" {
+      depends_on = [pagerduty_team_membership.test1]
+      team_ids = [pagerduty_team.test1.id]
     }
-`, teamname, username1, email1, username2, email2)
+    data "pagerduty_users" "test_by_2_team" {
+      depends_on = [pagerduty_team_membership.test2]
+      team_ids = [pagerduty_team.test1.id, pagerduty_team.test2.id]
+    }
+`, teamname1, teamname2, username1, email1, username2, email2, username3, email3)
 }

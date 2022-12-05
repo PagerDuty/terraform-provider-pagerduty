41e10f7bebd3d23a53e3aae144c25f8943ff612f
mrdubr <mr.dubr@gmail.com>
2022-12-05 15:32:12 -0500

    removed the A2A runner resource

--- pagerduty/data_source_pagerduty_automation_actions_runner_test.go
+++ pagerduty/data_source_pagerduty_automation_actions_runner_test.go
@@ -4,20 +4,19 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccDataSourcePagerDutyAutomationActionsRunner_Basic(t *testing.T) {
-	name := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	// name := fmt.Sprintf("tf-%s", acctest.RandString(5))
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourcePagerDutyAutomationActionsRunnerConfig(name),
+				Config: testAccDataSourcePagerDutyAutomationActionsRunnerConfig(),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckPagerdutyAutomationActionsRunnerExists("data.pagerduty_automation_actions_runner.foo"),
 				),
@@ -29,6 +28,7 @@ func TestAccDataSourcePagerDutyAutomationActionsRunner_Basic(t *testing.T) {
 func testAccCheckPagerdutyAutomationActionsRunnerExists(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		// client, _ := testAccProvider.Meta().(*Config).Client()
+		// client.AutomationActionsRunner.Create()
 
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
@@ -42,15 +42,6 @@ func testAccCheckPagerdutyAutomationActionsRunnerExists(n string) resource.TestC
 	}
 }
 
-func testAccDataSourcePagerDutyAutomationActionsRunnerConfig(name string) string {
-	return fmt.Sprintf(`
-resource "pagerduty_automation_actions_runner" "test" {
-  name = "%s"
-  runner_type = "sidecar"
-}
-
-data "pagerduty_automation_actions_runner" "foo" {
-  id = pagerduty_automation_actions_runner.test.id
-}
-`, name)
+func testAccDataSourcePagerDutyAutomationActionsRunnerConfig() string {
+	return fmt.Sprintf(``)
 }

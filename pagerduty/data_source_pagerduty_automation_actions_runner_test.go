10f9ef718936945837b882f328aaeb9598091879
mrdubr <mr.dubr@gmail.com>
2022-12-06 19:33:02 -0500

    datasource test

--- pagerduty/data_source_pagerduty_automation_actions_runner_test.go
+++ pagerduty/data_source_pagerduty_automation_actions_runner_test.go
@@ -7,96 +7,64 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/heimweh/go-pagerduty/pagerduty"
 )
 
 func TestAccDataSourcePagerDutyAutomationActionsRunner_Basic(t *testing.T) {
-	runnerName := fmt.Sprintf("tf-%s", acctest.RandString(5))
-	username := fmt.Sprintf("tf-%s", acctest.RandString(5))
-	email := fmt.Sprintf("%s@foo.test", username)
-	runnerId := "default"
+	name := fmt.Sprintf("tf-%s", acctest.RandString(5))
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourcePagerDutyAutomationActionsRunnerConfig_1(username, email),
+				Config: testAccDataSourcePagerDutyAutomationActionsRunnerConfig(name),
 				Check: resource.ComposeTestCheckFunc(
-					testAccDataSourcePagerdutyAutomationActionsRunnerRunnerCreation_1(runnerName, &runnerId),
-				),
-			},
-			{
-				Config: testAccDataSourcePagerDutyAutomationActionsRunnerConfig(runnerName, &runnerId),
-				Check: resource.ComposeTestCheckFunc(
-					testAccDataSourcePagerdutyAutomationActionsRunner("data.pagerduty_automation_actions_runner.foo"),
+					testAccDataSourcePagerdutyAutomationActionsRunner("pagerduty_automation_actions_runner.test", "data.pagerduty_automation_actions_runner.foo"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataSourcePagerdutyAutomationActionsRunner(n string) resource.TestCheckFunc {
+func testAccDataSourcePagerdutyAutomationActionsRunner(src, n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 
-		rs, ok := s.RootModule().Resources[n]
+		srcR := s.RootModule().Resources[src]
+		srcA := srcR.Primary.Attributes
+
+		r, ok := s.RootModule().Resources[n]
 		if !ok {
 			return fmt.Errorf("Not found: %s", n)
 		}
-		if rs.Primary.ID == "" {
+		a := r.Primary.Attributes
+
+		if a["id"] == "" {
 			return fmt.Errorf("No Runner ID is set")
 		}
 
+		testAtts := []string{"id", "name", "type", "runner_type", "creation_time", "last_seen", "description", "runbook_base_uri", "teams"}
+
+		for _, att := range testAtts {
+			if a[att] != srcA[att] {
+				return fmt.Errorf("Expected the runner %s to be: %s, but got: %s", att, srcA[att], a[att])
+			}
+		}
+
 		return nil
 	}
 }
 
-func testAccDataSourcePagerDutyAutomationActionsRunnerConfig(runnerName string, runnerId *string) string {
-
-	fmt.Println("testAccDataSourcePagerDutyAutomationActionsRunnerConfig runnerId:", *runnerId)
-
+func testAccDataSourcePagerDutyAutomationActionsRunnerConfig(name string) string {
 	return fmt.Sprintf(`
-data "pagerduty_automation_actions_runner" "tf_test_runner" {
-  id = %q
-}
-`, *runnerId)
-}
-
-func testAccDataSourcePagerDutyAutomationActionsRunnerConfig_1(username, email string) string {
-
-	fmt.Println("Called: testAccDataSourcePagerDutyAutomationActionsRunnerConfig_1")
-
-	return fmt.Sprintf(`
-resource "pagerduty_user" "foo" {
-	name        = "%s"
-	email       = "%s"
-	color       = "green"
-	role        = "user"
-	job_title   = "foo"
-	description = "foo"
-}
-`, username, email)
+resource "pagerduty_automation_actions_runner" "test" {
+  name = "%s"
+  runner_type = "runbook"
+  runbook_base_uri = "cat-cat"
+  runbook_api_key = "secret"
 }
 
-func testAccDataSourcePagerdutyAutomationActionsRunnerRunnerCreation_1(runnerName string, runnerId *string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-
-		fmt.Println("testAccDataSourcePagerdutyAutomationActionsRunnerRunnerCreation_1 runnerId:", *runnerId)
-
-		client, _ := testAccProvider.Meta().(*Config).Client()
-		input := &pagerduty.AutomationActionsRunner{
-			Name:        runnerName,
-			Description: "sidecar runner provisioned for tf acceptance test",
-			RunnerType:  "sidecar",
-		}
-		runner, _, err := client.AutomationActionsRunner.Create(input)
-		if err != nil {
-			return err
-		}
-		*runnerId = runner.ID
-
-		fmt.Println("POST testAccDataSourcePagerdutyAutomationActionsRunnerRunnerCreation_1 runnerId:", *runnerId)
-
-		return nil
-	}
+data "pagerduty_automation_actions_runner" "foo" {
+  id = pagerduty_automation_actions_runner.test.id
+}
+`, name)
 }

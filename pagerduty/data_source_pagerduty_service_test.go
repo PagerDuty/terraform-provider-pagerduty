88dbe72547972069c51d1640d685828e464fba2a
adamo57 <adamouellette57@gmail.com>
2023-03-26 17:25:09 -0500

    Separate tests and add more use cases

--- pagerduty/data_source_pagerduty_service_test.go
+++ pagerduty/data_source_pagerduty_service_test.go
@@ -23,9 +23,50 @@ func TestAccDataSourcePagerDutyService_Basic(t *testing.T) {
 			{
 				Config: testAccDataSourcePagerDutyServiceConfig(username, email, service, escalationPolicy, teamname),
 				Check: resource.ComposeTestCheckFunc(
-					testAccDataSourcePagerDutyService("pagerduty_service.test", "data.pagerduty_service.by_name"),
-					resource.TestCheckResourceAttr("data.pagerduty_service.by_name", "teams.#", "1"),
-					resource.TestCheckResourceAttr("data.pagerduty_service.by_name", "teams.0.name", teamname),
+					testAccDataSourcePagerDutyService("pagerduty_service.no_team_service", "data.pagerduty_service.no_team_service"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourcePagerDutyService_HasNoTeam(t *testing.T) {
+	username := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	email := fmt.Sprintf("%s@foo.test", username)
+	service := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	escalationPolicy := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	teamname := fmt.Sprintf("tf-%s", acctest.RandString(5))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourcePagerDutyServiceConfig(username, email, service, escalationPolicy, teamname),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.pagerduty_service.no_team_service", "teams.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourcePagerDutyService_HasOneTeam(t *testing.T) {
+	username := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	email := fmt.Sprintf("%s@foo.test", username)
+	service := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	escalationPolicy := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	teamname := fmt.Sprintf("tf-%s", acctest.RandString(5))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourcePagerDutyServiceConfig(username, email, service, escalationPolicy, teamname),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.pagerduty_service.one_team_service", "teams.#", "1"),
+					resource.TestCheckResourceAttr("data.pagerduty_service.one_team_service", "teams.0.name", teamname),
 				),
 			},
 		},
@@ -45,7 +86,7 @@ func testAccDataSourcePagerDutyService(src, n string) resource.TestCheckFunc {
 			return fmt.Errorf("Expected to get a service ID from PagerDuty")
 		}
 
-		testAtts := []string{"teams", "id", "name", "type", "auto_resolve_timeout", "acknowledgement_timeout", "alert_creation", "description", "escalation_policy"}
+		testAtts := []string{"id", "name", "type", "auto_resolve_timeout", "acknowledgement_timeout", "alert_creation", "description", "escalation_policy"}
 
 		for _, att := range testAtts {
 			if a[att] != srcA[att] {
@@ -59,9 +100,9 @@ func testAccDataSourcePagerDutyService(src, n string) resource.TestCheckFunc {
 
 func testAccDataSourcePagerDutyServiceConfig(username, email, service, escalationPolicy, teamname string) string {
 	return fmt.Sprintf(`
-resource "pagerduty_team" "test" {
+resource "pagerduty_team" "team_one" {
 	name        = "%s"
-	description = "test"
+	description = "team_one"
 }
 
 resource "pagerduty_user" "test" {
@@ -69,15 +110,30 @@ resource "pagerduty_user" "test" {
   email = "%s"
 }
 
-resource "pagerduty_team_membership" "test" {
-	team_id = pagerduty_team.test.id
+resource "pagerduty_team_membership" "team_membership_one" {
+	depends_on = [pagerduty_team.team_one, pagerduty_user.test]
+	team_id = pagerduty_team.team_one.id
 	user_id = pagerduty_user.test.id
 }
 
-resource "pagerduty_escalation_policy" "test" {
+resource "pagerduty_escalation_policy" "no_team_ep" {
+	name        = "no_team_ep"
+	num_loops   = 2
+	rule {
+	  escalation_delay_in_minutes = 10
+	  target {
+		type = "user_reference"
+		id   = pagerduty_user.test.id
+	  }
+	}
+  }
+
+resource "pagerduty_escalation_policy" "one_team_ep" {
+
+  depends_on = [pagerduty_team_membership.team_membership_one, pagerduty_user.test]
   name        = "%s"
   num_loops   = 2
-  teams       = [pagerduty_team.test.id]
+  teams       = [pagerduty_team.team_one.id]
   rule {
     escalation_delay_in_minutes = 10
     target {
@@ -87,17 +143,34 @@ resource "pagerduty_escalation_policy" "test" {
   }
 }
 
-resource "pagerduty_service" "test" {
+resource "pagerduty_service" "no_team_service" {
+	depends_on = [pagerduty_escalation_policy.no_team_ep]
+	name                    = "no_team_service"
+	auto_resolve_timeout    = 14400
+	acknowledgement_timeout = 600
+	escalation_policy       = pagerduty_escalation_policy.no_team_ep.id
+	alert_creation          = "create_incidents"
+}
+
+
+resource "pagerduty_service" "one_team_service" {
+  depends_on = [pagerduty_escalation_policy.one_team_ep]
   name                    = "%s"
   auto_resolve_timeout    = 14400
   acknowledgement_timeout = 600
-  escalation_policy       = pagerduty_escalation_policy.test.id
+  escalation_policy       = pagerduty_escalation_policy.one_team_ep.id
   alert_creation          = "create_incidents"
 }
 
-data "pagerduty_service" "by_name" {
-  depends_on = [pagerduty_team_membership.test, pagerduty_service.test]
-  name = pagerduty_service.test.name
+data "pagerduty_service" "no_team_service" {
+	depends_on = [pagerduty_service.no_team_service]
+	name = pagerduty_service.no_team_service.name
 }
+
+data "pagerduty_service" "one_team_service" {
+  depends_on = [pagerduty_service.one_team_service]
+  name = pagerduty_service.one_team_service.name
+}
+
 `, teamname, username, email, service, escalationPolicy)
 }

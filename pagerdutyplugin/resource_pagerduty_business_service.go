63f796df94ca3d6aab7443bea29e9b2a5f4c6008
Carlos Gajardo <cjavier@pagerduty.com>
2024-02-22 10:03:20 -0300

    Improve resource business service code organization

--- pagerdutyplugin/resource_pagerduty_business_service.go
+++ pagerdutyplugin/resource_pagerduty_business_service.go
@@ -2,12 +2,13 @@ package pagerduty
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/PagerDuty/go-pagerduty"
-	"github.com/PagerDuty/terraform-provider-pagerduty/util"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
@@ -47,7 +48,7 @@ func (r *resourceBusinessService) Schema(_ context.Context, _ resource.SchemaReq
 				Optional:           true,
 				Computed:           true,
 				Default:            stringdefault.StaticString("business_service"),
-				DeprecationMessage: "This will change to a computed resource in the next major release.",
+				DeprecationMessage: "This will become a computed attribute in the next major release.",
 				Validators:         []validator.String{stringvalidator.OneOf("business_service")},
 			},
 			"point_of_contact": schema.StringAttribute{Optional: true},
@@ -58,14 +59,15 @@ func (r *resourceBusinessService) Schema(_ context.Context, _ resource.SchemaReq
 
 func (r *resourceBusinessService) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
 	var plan resourceBusinessServiceModel
+
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 	businessServicePlan := buildPagerdutyBusinessService(ctx, &plan)
+	log.Printf("[INFO] Creating PagerDuty business service %s", plan.Name)
 
 	err := helperResource.RetryContext(ctx, 5*time.Minute, func() *helperResource.RetryError {
-		log.Printf("[INFO] Creating PagerDuty business service %s", plan.Name)
 		bs, err := r.client.CreateBusinessServiceWithContext(ctx, businessServicePlan)
 		if err != nil {
 			return helperResource.NonRetryableError(err)
@@ -76,25 +78,14 @@ func (r *resourceBusinessService) Create(ctx context.Context, req resource.Creat
 	})
 	if err != nil {
 		resp.Diagnostics.AddError(
-			"Error calling CreateBusinessServiceWithContext",
+			fmt.Sprintf("Error creating Business Service %s", plan.Name),
 			err.Error(),
 		)
 		return
 	}
 
-	err = helperResource.RetryContext(ctx, 5*time.Minute, func() *helperResource.RetryError {
-		businessService, err := r.client.GetBusinessServiceWithContext(ctx, businessServicePlan.ID)
-		if err != nil {
-			return helperResource.RetryableError(err)
-		}
-		plan = buildResourceBusinessServiceModel(businessService)
-		return nil
-	})
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Error calling GetBusinessServiceWithContext",
-			err.Error(),
-		)
+	plan = requestGetBusinessService(ctx, r.client, businessServicePlan.ID, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
 		return
 	}
 	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
@@ -102,27 +93,15 @@ func (r *resourceBusinessService) Create(ctx context.Context, req resource.Creat
 
 func (r *resourceBusinessService) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
 	var state resourceBusinessServiceModel
+
 	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
-	log.Printf("[INFO] Reading PagerDuty business service %s", state.ID)
 	if resp.Diagnostics.HasError() {
 		return
 	}
-	err := helperResource.RetryContext(ctx, 5*time.Minute, func() *helperResource.RetryError {
-		businessService, err := r.client.GetBusinessServiceWithContext(ctx, state.ID.ValueString())
-		if err != nil {
-			if util.IsBadRequestError(err) {
-				return helperResource.NonRetryableError(err)
-			}
-			return helperResource.RetryableError(err)
-		}
-		state = buildResourceBusinessServiceModel(businessService)
-		return nil
-	})
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Error calling GetBusinessServiceWithContext",
-			err.Error(),
-		)
+	log.Printf("[INFO] Reading PagerDuty business service %s", state.ID)
+
+	state = requestGetBusinessService(ctx, r.client, state.ID.ValueString(), &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
 		return
 	}
 	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
@@ -130,6 +109,7 @@ func (r *resourceBusinessService) Read(ctx context.Context, req resource.ReadReq
 
 func (r *resourceBusinessService) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
 	var plan resourceBusinessServiceModel
+
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
 	if resp.Diagnostics.HasError() {
 		return
@@ -141,26 +121,24 @@ func (r *resourceBusinessService) Update(ctx context.Context, req resource.Updat
 		req.State.GetAttribute(ctx, path.Root("id"), &id)
 		businessServicePlan.ID = id
 	}
-
-	log.Printf("[DEBUG] poc: %v", businessServicePlan.PointOfContact)
-	log.Printf("[DEBUG] point_of_contact: %v", plan.PointOfContact)
-	log.Printf("[INFO] Updating PagerDuty business service %s", plan.ID)
+	log.Printf("[INFO] Updating PagerDuty business service %s", businessServicePlan.ID)
 
 	businessService, err := r.client.UpdateBusinessServiceWithContext(ctx, businessServicePlan)
 	if err != nil {
 		resp.Diagnostics.AddError(
-			"Error calling UpdateBusinessServiceWithContext",
+			fmt.Sprintf("Error updating Business Service %s", businessServicePlan.ID),
 			err.Error(),
 		)
 		return
 	}
-	plan = buildResourceBusinessServiceModel(businessService)
+	plan = flattenBusinessService(businessService)
 
 	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
 }
 
 func (r *resourceBusinessService) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
 	var id types.String
+
 	resp.Diagnostics.Append(req.State.GetAttribute(ctx, path.Root("id"), &id)...)
 	if resp.Diagnostics.HasError() {
 		return
@@ -170,7 +148,7 @@ func (r *resourceBusinessService) Delete(ctx context.Context, req resource.Delet
 	err := r.client.DeleteBusinessServiceWithContext(ctx, id.ValueString())
 	if err != nil {
 		resp.Diagnostics.AddError(
-			"Error calling DeleteBusinessServiceWithContext",
+			fmt.Sprintf("Error deleting Business Service %s", id),
 			err.Error(),
 		)
 		return
@@ -188,43 +166,69 @@ func (r *resourceBusinessService) ImportState(ctx context.Context, req resource.
 
 type resourceBusinessServiceModel struct {
 	ID             types.String `tfsdk:"id"`
-	Name           types.String `tfsdk:"name"`
 	Description    types.String `tfsdk:"description"`
-	Type           types.String `tfsdk:"type"`
-	Summary        types.String `tfsdk:"summary"`
-	Self           types.String `tfsdk:"self"`
-	PointOfContact types.String `tfsdk:"point_of_contact"`
 	HTMLUrl        types.String `tfsdk:"html_url"`
+	Name           types.String `tfsdk:"name"`
+	PointOfContact types.String `tfsdk:"point_of_contact"`
+	Self           types.String `tfsdk:"self"`
+	Summary        types.String `tfsdk:"summary"`
 	Team           types.String `tfsdk:"team"`
+	Type           types.String `tfsdk:"type"`
 }
 
-func buildResourceBusinessServiceModel(src *pagerduty.BusinessService) resourceBusinessServiceModel {
+func requestGetBusinessService(ctx context.Context, client *pagerduty.Client, id string, diags *diag.Diagnostics) resourceBusinessServiceModel {
 	var model resourceBusinessServiceModel
-	model.ID = types.StringValue(src.ID)
-	model.Name = types.StringValue(src.Name)
-	model.HTMLUrl = types.StringValue(src.HTMLUrl)
-	model.Description = types.StringValue(src.Description)
-	model.Type = types.StringValue(src.Type)
-	model.PointOfContact = types.StringValue(src.PointOfContact)
-	model.Summary = types.StringValue(src.Summary)
-	model.Self = types.StringValue(src.Self)
-	if src.Team != nil {
-		model.Team = types.StringValue(src.Team.ID)
+
+	err := helperResource.RetryContext(ctx, 5*time.Minute, func() *helperResource.RetryError {
+		businessService, err := client.GetBusinessServiceWithContext(ctx, id)
+		if err != nil {
+			return helperResource.RetryableError(err)
+		}
+		model = flattenBusinessService(businessService)
+		return nil
+	})
+	if err != nil {
+		diags.AddError(
+			fmt.Sprintf("Error reading Business Service %s", id),
+			err.Error(),
+		)
 	}
+
 	return model
 }
 
 func buildPagerdutyBusinessService(ctx context.Context, model *resourceBusinessServiceModel) *pagerduty.BusinessService {
 	businessService := pagerduty.BusinessService{
 		ID:             model.ID.ValueString(),
-		Name:           model.Name.ValueString(),
 		Description:    model.Description.ValueString(),
-		Type:           model.Type.ValueString(),
-		Summary:        model.Summary.ValueString(),
-		Self:           model.Self.ValueString(),
-		PointOfContact: model.PointOfContact.ValueString(),
 		HTMLUrl:        model.HTMLUrl.ValueString(),
+		Name:           model.Name.ValueString(),
+		PointOfContact: model.PointOfContact.ValueString(),
+		Self:           model.Self.ValueString(),
+		Summary:        model.Summary.ValueString(),
 		Team:           &pagerduty.BusinessServiceTeam{ID: model.Team.ValueString()},
+		Type:           model.Type.ValueString(),
 	}
 	return &businessService
+}
+
+func flattenBusinessService(src *pagerduty.BusinessService) resourceBusinessServiceModel {
+	model := resourceBusinessServiceModel{
+		ID:             types.StringValue(src.ID),
+		Description:    types.StringValue(src.Description),
+		HTMLUrl:        types.StringValue(src.HTMLUrl),
+		Name:           types.StringValue(src.Name),
+		Self:           types.StringValue(src.Self),
+		Summary:        types.StringValue(src.Summary),
+		Type:           types.StringValue(src.Type),
+		PointOfContact: types.StringNull(),
+		Team:           types.StringNull(),
+	}
+	if src.PointOfContact != "" {
+		model.PointOfContact = types.StringValue(src.PointOfContact)
+	}
+	if src.Team != nil {
+		model.Team = types.StringValue(src.Team.ID)
+	}
+	return model
 }
--- util/http_util.go
+++ util/http_util.go
@@ -19,12 +19,17 @@ func IsBadRequestError(err error) bool {
 var notFoundErrorRegexp = regexp.MustCompile(".*: 404 Not Found$")
 
 func IsNotFoundError(err error) bool {
+	if err == nil {
+		return false
+	}
+
 	var apiErr pagerduty.APIError
 	if errors.As(err, &apiErr) {
 		if apiErr.StatusCode == http.StatusNotFound {
 			return true
 		}
 	}
+
 	// There are some errors that doesn't stick to expected error interface
 	// and fallback to a simple text error message that can be capture by
 	// this regexp.

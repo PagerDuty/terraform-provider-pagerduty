e3a9d86de58a6e7abc0ba5a25b393db0e51c7e6d
Carlos Gajardo <cjavier@pagerduty.com>
2024-03-19 22:24:21 -0300

    Migrate data source vendor

--- pagerduty/data_source_pagerduty_vendor.go
+++ pagerduty/data_source_pagerduty_vendor.go
@@ -13,6 +13,7 @@ import (
 	"github.com/heimweh/go-pagerduty/pagerduty"
 )
 
+// Deprecated: Migrated to pagerdutyplugin.resourceVendor. Kept for testing purposes.
 func dataSourcePagerDutyVendor() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourcePagerDutyVendorRead,
--- pagerdutyplugin/provider.go
+++ pagerdutyplugin/provider.go
@@ -68,6 +68,7 @@ func (p *Provider) DataSources(_ context.Context) [](func() datasource.DataSourc
 		func() datasource.DataSource { return &dataSourceStandardsResourcesScores{} },
 		func() datasource.DataSource { return &dataSourceStandards{} },
 		func() datasource.DataSource { return &dataSourceTag{} },
+		func() datasource.DataSource { return &dataSourceVendor{} },
 	}
 }
 

ed044cd578dc933193eb9b153eb9cce255e2e7ed
Alena Pantuzenko <apantuzenko@pagerduty.com>
2024-04-08 10:19:26 -0400

    delete unused import

--- pagerduty/resource_pagerduty_event_orchestration_integration.go
+++ pagerduty/resource_pagerduty_event_orchestration_integration.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"

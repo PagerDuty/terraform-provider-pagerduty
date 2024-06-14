8fb9df889c17a44109059e6b1eb08ea709a85f08
José Antonio Reyes <imjaroiswebdev@gmail.com>
2024-06-14 12:57:17 -0400

    add docs update and minimal tweaks

--- pagerduty/resource_pagerduty_service.go
+++ pagerduty/resource_pagerduty_service.go
@@ -351,6 +351,9 @@ func customizePagerDutyServiceDiff(context context.Context, diff *schema.Resourc
 		if agpType == "content_based" && (aggregateVal == "" || len(fieldsVal) == 0) {
 			return fmt.Errorf("When using Alert grouping parameters configuration of type \"content_based\" is in use, attributes \"aggregate\" and \"fields\" are required")
 		}
+		if timeWindowVal == 86400 && agpType != "content_based" {
+			return fmt.Errorf("Alert grouping parameters configuration attribute \"time_window\" with a value of 86400 is only supported by \"content-based\" type Alert Grouping")
+		}
 		if (aggregateVal != "" || len(fieldsVal) > 0) && (agpType != "" && hasChangeAgpType && agpType != "content_based") {
 			return fmt.Errorf("Alert grouping parameters configuration attributes \"aggregate\" and \"fields\" are only supported by \"content_based\" type Alert Grouping")
 		}
@@ -359,9 +362,6 @@ func customizePagerDutyServiceDiff(context context.Context, diff *schema.Resourc
 		}
 		if (timeWindowVal > 300) && (agpType != "" && hasChangeAgpType && (agpType != "intelligent" && agpType != "content_based")) {
 			return fmt.Errorf("Alert grouping parameters configuration attribute \"time_window\" is only supported by \"intelligent\" and \"content-based\" type Alert Grouping")
-		}
-		if timeWindowVal == 86400 && agpType != "content_based" {
-			return fmt.Errorf("Alert grouping parameters configuration attribute \"time_window\" with a value of 86400 is only supported by \"content-based\" type Alert Grouping")
 		}
 	}
 

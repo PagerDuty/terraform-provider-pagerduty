ea3d0952e4b8073addf7d5a9a35683a749c3deff
Lorenzo Davis <>
2024-07-03 16:45:50 +0400

    Working reproduction of EP Management problem

--- pagerduty/resource_pagerduty_team_membership_test.go
+++ pagerduty/resource_pagerduty_team_membership_test.go
@@ -297,3 +297,223 @@ resource "pagerduty_escalation_policy" "foo" {
 }
 `, user, team, role, escalationPolicy)
 }
+
+func TestAccPagerDutyTeamMembership_basic(t *testing.T) { // from gpt and modified
+	user := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	team1 := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	team2 := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	role := "manager"
+	escalationPolicy1 := fmt.Sprintf("tf-%s", acctest.RandString(5))
+	escalationPolicy2 := fmt.Sprintf("tf-%s", acctest.RandString(5))
+
+	fmt.Print("starting tests\n")
+	fmt.Printf("starting tests\n\n")
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckPagerDutyTeamMembershipDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckPagerDutyTeamMembershipDestroyWithEscalationPolicyDependant_UnrelatedEPs(user, team1, team2, role, escalationPolicy1, escalationPolicy2),
+				Check: resource.ComposeTestCheckFunc(
+					printdebug("checkpoint1-1\n\n"),
+					testAccCheckPagerDutyTeamMembershipExists("pagerduty_team_membership.foo"),
+					printdebug("checkpoint1-2\n\n"),
+
+					testAccCheckPagerDutyTeamMembershipExists("pagerduty_team_membership.bar"),
+					printdebug("checkpoint1-3\n\n"),
+
+					resource.TestCheckResourceAttr("pagerduty_team_membership.bar", "role", "manager"),
+					printdebug("checkpoint1-4\n\n"),
+				),
+			},
+			{
+				// ResourceName: "pagerduty_team_membership.foo",
+				// ImportState:  true,
+				// RefreshState: true,
+				Config: testAccCheckPagerDutyTeamMembershipDestroyWithEscalationPolicyDependant_UnrelatedEPsUpdated(user, team1, team2, role, escalationPolicy1, escalationPolicy2),
+				Check: resource.ComposeTestCheckFunc(
+					printdebug("checkpoint2-1\n\n"),
+
+					testAccCheckPagerDutyTeamMembershipDestroyState("pagerduty_team_membership.foo"),
+					printdebug("checkpoint2-2\n\n"),
+
+					testAccCheckPagerDutyTeamExists("pagerduty_team.foobar"),
+					printdebug("checkpoint2-3\n\n"),
+
+					testAccCheckPagerDutyEscalationPolicyExists("pagerduty_escalation_policy.foo"),
+					printdebug("checkpoint2-4\n\n"),
+
+					testAccCheckPagerDutyEscalationPolicyExists("pagerduty_escalation_policy.bar"),
+					printdebug("checkpoint2-5\n\n"),
+
+					testAccCheckPagerDutyEscalationPolicyTeamsFieldMatches("pagerduty_escalation_policy.foo", "foo"),
+					printdebug("checkpoint2-6\n\n"),
+
+					testAccCheckPagerDutyEscalationPolicyTeamsFieldMatches("pagerduty_escalation_policy.bar", "bar"),
+					printdebug("checkpoint2-7\n\n"),
+				),
+			},
+		},
+	})
+}
+
+func printdebug(msg string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		fmt.Printf(msg)
+		return nil
+	}
+}
+
+func testAccCheckPagerDutyEscalationPolicyTeamsFieldMatches(n, expectedTeam string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		teams := rs.Primary.Attributes["teams.#"]
+		if teams != "1" {
+			return fmt.Errorf("Expected teams to have 1 element, got: %s", teams)
+		}
+
+		team := rs.Primary.Attributes["teams.0"]
+		if team != expectedTeam {
+			return fmt.Errorf("Expected team to be %s, got: %s", expectedTeam, team)
+		}
+
+		return fmt.Errorf("dummy error")
+		// return nil
+	}
+}
+
+func testAccCheckPagerDutyTeamMembershipDestroyState(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID != "" {
+			return fmt.Errorf("Resource still exists: %s", n)
+		}
+		return fmt.Errorf("dummy error: %s", "wow")
+		// return nil
+	}
+}
+
+func testAccCheckPagerDutyTeamMembershipDestroyWithEscalationPolicyDependant_UnrelatedEPs(user, team1, team2, role, escalationPolicy1, escalationPolicy2 string) string {
+	return fmt.Sprintf(`
+resource "pagerduty_user" "foo" {
+  name = "%[1]v"
+  email = "%[1]v@foo.test"
+}
+
+resource "pagerduty_team" "foo" {
+  name        = "%[2]v"
+  description = "foo"
+}
+
+resource "pagerduty_team" "bar" {
+  name        = "%[5]v"
+  description = "bar"
+}
+
+resource "pagerduty_team_membership" "foo" {
+  user_id = data.pagerduty_user.foo.id
+  team_id = pagerduty_team.foo.id
+  role    = "%[3]v"
+}
+
+resource "pagerduty_team_membership" "bar" {
+  user_id = data.pagerduty_user.foo.id
+  team_id = pagerduty_team.bar.id
+  role    = "%[3]v"
+}
+
+resource "pagerduty_escalation_policy" "foo" {
+  name      = "%[4]v"
+  num_loops = 2
+  teams     = [pagerduty_team.foo.id]
+  description = "foo"
+
+  rule {
+    escalation_delay_in_minutes = 10
+    target {
+      type = "user_reference"
+      id   = data.pagerduty_user.foo.id
+    }
+  }
+}
+
+resource "pagerduty_escalation_policy" "bar" {
+  name      = "%[6]v"
+  num_loops = 2
+  teams     = [pagerduty_team.bar.id]
+  description = "bar"
+
+  rule {
+    escalation_delay_in_minutes = 10
+    target {
+      type = "user_reference"
+      id   = data.pagerduty_user.foo.id
+    }
+  }
+}
+`, user, team1, role, escalationPolicy1, team2, escalationPolicy2)
+}
+
+func testAccCheckPagerDutyTeamMembershipDestroyWithEscalationPolicyDependant_UnrelatedEPsUpdated(user, team1, team2, role, escalationPolicy1, escalationPolicy2 string) string {
+	return fmt.Sprintf(`
+resource "pagerduty_user" "foo" {
+  name = "%[1]v"
+  email = "%[1]v@foo.test"
+}
+
+resource "pagerduty_team" "foo" {
+  name        = "%[2]v"
+  description = "foo"
+}
+
+resource "pagerduty_team" "bar" {
+  name        = "%[5]v"
+  description = "bar"
+}
+
+// now the user should be on a single team
+resource "pagerduty_team_membership" "bar" {
+  user_id = data.pagerduty_user.foo.id
+  team_id = pagerduty_team.bar.id
+  role    = "%[3]v"
+}
+
+resource "pagerduty_escalation_policy" "foo" {
+  name      = "%[4]v"
+  num_loops = 2
+  teams     = [pagerduty_team.foo.id]
+
+  rule {
+    escalation_delay_in_minutes = 10
+    target {
+      type = "user_reference"
+      id   = data.pagerduty_user.foo.id
+    }
+  }
+}
+
+resource "pagerduty_escalation_policy" "bar" {
+  name      = "%[6]v"
+  num_loops = 2
+  description = "bar"
+  teams     = [pagerduty_team.bar.id]
+
+  rule {
+    escalation_delay_in_minutes = 10
+    target {
+      type = "user_reference"
+      id   = data.pagerduty_user.foo.id
+    }
+  }
+}
+`, user, team1, role, escalationPolicy1, team2, escalationPolicy2)
+}
